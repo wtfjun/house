2b60341f10624077838ae85bd0bbe280223f9579
cxj <cxj@cxjdeMacBook-Pro.local>
2016-12-12 12:38:29 +0800

    增加底部

--- src/js/containers/Home.js
+++ src/js/containers/Home.js
@@ -21,6 +21,12 @@ export default class Home extends Component {
 			    <div className="section">
 			    	<img src='https://www.houseofhackney.com/media/wysiwyg/HP_ALL_Desktop_.jpg' />
 			    </div>
+			    <div className="footer section fp-auto-height">
+			    		SUBSCRIBE TO OUR NEWSLETTER
+			    		<br />
+			    		<p>Find us: House of Hackney Flagship Store, 131 Shoreditch High Street. London. E1 6JE Call us +44 (0) 207 613 5559
+			    		</p>
+			    </div>
 				</div>
 			</div>
     );
